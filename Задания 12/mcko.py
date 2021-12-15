7ac6b69b973c4c7da31a11047816b2e9529d6786
Velimirka <64539846+Velimirka@users.noreply.github.com>
2021-12-15 14:10:33 +0300

    Update mcko.py
    убрал другое задание из кода

--- Задания 12/mcko.py
+++ Задания 12/mcko.py
@@ -1,15 +1,9 @@
-# s = '1' * 70
-#
-# while '1111' in s or '2222' in s:
-#
-#     if '1111' in s:
-#         s = s.replace('1111', '22')
-#     else:
-#         s = s.replace('2222', '11')
-# print(s)
+s = '1' * 70
 
+while '1111' in s or '2222' in s:
 
-
-a = 10 ** 2019 - 10 ** 1019 + 10 ** 1200 - 10 ** 3
-s = str(a).count('9')
+    if '1111' in s:
+        s = s.replace('1111', '22')
+    else:
+        s = s.replace('2222', '11')
 print(s)
